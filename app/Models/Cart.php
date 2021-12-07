414f631e2c7e881abde0134936c6c7984a2732ac
Breakermind <marcin@breakermind.com>
2021-12-07 12:56:29 +0100

    Models

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -25,8 +25,8 @@ class Cart extends Model
 	];
 
 	protected $casts = [
-        'created_at' => 'datetime:Y-m-d h:i:s',
-    ];
+		'created_at' => 'datetime:Y-m-d h:i:s',
+	];
 
 	function variants()
 	{
@@ -43,5 +43,74 @@ class Cart extends Model
 			return new CartVariant($attributes);
 		}
 		return parent::pivot($parent, $attributes, $table, $exists);
+	}
+
+	function addProduct($id,$qty = 1)
+	{
+		if($id > 0 && $qty > 1) {
+			$this->variants()->attach([$id => ['quantity' => $qty]]);
+		}
+	}
+
+	function delProduct($id)
+	{
+		if($id > 0) {
+			$this->variants()->wherePivot('id', $id)->detach();
+		}
+	}
+
+	function plusProduct($id)
+	{
+		$p = $this->variants()->find((int) $id);
+		if($p) {
+			$p->pivot->increment('quantity', 1);
+
+		}
+	}
+
+	function minusProduct($id)
+	{
+		$p = $this->variants()->find((int) $id);
+		if($p) {
+			if($p->pivot->quantity >= 2) {
+				$p->pivot->decrement('quantity', 1);
+			}
+		}
+	}
+
+	function addAddon($cart_id,$addon_id,$qty = 1)
+	{
+		if($cart_id > 0 && $addon_id > 0 && $qty >= 1) {
+			$this->variants()->wherePivot('id', $cart_id)->first()->pivot
+			->addons()->sync([$addon_id => ['quantity' => $qty]], false);
+		}
+	}
+
+	function delAddon($cart_id, $id)
+	{
+		if($cart_id > 0 && $id > 0) {
+			$this->variants()->wherePivot('id', $cart_id)->first()->pivot
+			->addons()->wherePivot('id', $id)->detach();
+		}
+	}
+
+	function plusAddon($cart_id, $id)
+	{
+		if($cart_id > 0 && $id > 0) {
+			$p = $this->variants()->wherePivot('id', $cart_id)->first()->pivot
+			->addons()->wherePivot('id', $id)->first()->pivot;
+			$p->increment('quantity', 1);
+		}
+	}
+
+	function minusAddon($cart_id, $id)
+	{
+		if($cart_id > 0 && $id > 0) {
+			$p = $this->variants()->wherePivot('id', $cart_id)->first()->pivot
+			->addons()->wherePivot('id', $id)->first()->pivot;
+			if($p->quantity > 1) {
+				$p->decrement('quantity', 1);
+			}
+		}
 	}
 }
